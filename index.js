d1e6398d4416d52d4c4f260407301cefe0a99219
usa88722 <12369463-usa88722@users.noreply.replit.com>
2024-01-08 15:50:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,328 @@
+const fs = require('fs')
+const {Blob, FormData} = require('formdata-node')
+const Tesseract = require('tesseract.js');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+require('dotenv').config();
+const Botly = require("botly");
+const fetch = require("node-fetch");
+const PageID = "208135635708754";
+const { Sequelize } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+/*--------- SQ database ---------*/
+const sequelize = new Sequelize('database', 'user', 'pass', {
+  dialect: 'sqlite',
+  logging: false,
+  host: './db.sqlite'
+})
+sequelize.sync();
+class User extends Model {}
+User.init({uid: {type: DataTypes.STRING}, lang: {type: DataTypes.STRING}}, {sequelize, modelName: 'users', timestamps: false}); 
+/*--------- page database ---------*/
+const botly = new Botly({
+  accessToken: process.env.PAGE_ACCESS_TOKEN,
+  verifyToken: process.env.VERIFY_TOKEN,
+  webHookPath: process.env.WB_PATH,
+  notificationType: Botly.CONST.REGULAR,
+  FB_URL: "https://graph.facebook.com/v18.0/",
+});
+/*--------- Functions ---------*/
+async function updateOrCreate (model, where, newItem) {
+   const foundItem = await model.findOne({where});
+   if (!foundItem) {
+        const item = await model.create(newItem)
+        return  {item, created: true};
+    }
+    const item = await model.update(newItem, {where});
+    return {item, created: false};
+}
+/*--------- Functions ---------*/
+app.get("/", function (_req, res) {
+  res.sendStatus(200);
+});
+app.use(
+  bodyParser.json({
+    verify: botly.getVerifySignature(process.env.APP_SECRET),
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/webhook", botly.router());
+botly.on("message", async (senderId, message, data) => {
+  //aaaaaaaaaaaaa
+  botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.MARK_SEEN});
+  botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.TYPING_ON});
+
+  /*--------- s t a r t ---------*/
+  const user = await User.findOne({ where: { uid: senderId}});
+  if (message.message.text) {
+    if (user != null) {
+        fetch(`https://050322c2-3ffa-4316-9fb0-b2df32aa6ca3-00-2gyq3gw1w88n1.worf.replit.dev/translate.php?lang=${user.dataValues.lang}&text=${message.message.text}`)
+        .then(response => response.json())
+        .then(data => {
+            botly.sendText({id: senderId, text: data.result,
+            quick_replies: [
+                botly.createQuickReply("إضغط لتغيير اللغة 🔁", "ChangeLang")]})
+                }).catch(err => {console.error(err)});
+      } else {
+        await User.create({ uid: senderId, lang: "en" });
+        fetch(`https://050322c2-3ffa-4316-9fb0-b2df32aa6ca3-00-2gyq3gw1w88n1.worf.replit.dev/translate.php?lang=en&text=${message.message.text}`)
+        .then(response => response.json())
+        .then(data => {
+            botly.sendText({id: senderId, text: data.result,
+            quick_replies: [
+                botly.createQuickReply("إضغط لتغيير اللغة 🔁", "ChangeLang")]})
+                }).catch(err => {console.error(err)});
+      }
+    } else if (message.message.attachments[0].payload.sticker_id) {
+      botly.sendText({id: senderId, text: "(Y)"}) ;
+    } else if (message.message.attachments[0].type == "image") {
+        const attachment = message.message.attachments[0] 
+        const images = attachment.payload.url;
+
+      botly.sendText({id: senderId, text: "الميزة قيد التطوير \nانتظر قليلا😄⌛\nقد استغرق وقتا أطول لترجمة صورتك"});
+Tesseract.recognize(images, 'ara+eng+fra+deu+rus+ita+tur+kor+jpn+sqi+swe+hin+spa') 
+      .then(result => {
+const texts = result.data.text
+fetch(`https://050322c2-3ffa-4316-9fb0-b2df32aa6ca3-00-2gyq3gw1w88n1.worf.replit.dev/translate.php?lang=${user.dataValues.lang}&text=${texts}`)
+  .then(response => response.json())
+  .then(data => {
+      botly.sendText({id: senderId, text: data.result,
+      quick_replies: [
+          botly.createQuickReply("إضغط لتغيير اللغة 🔁", "ChangeLang")]})
+          }).catch(err => {console.error(err)});})
+    } else if (message.message.attachments[0].type == "audio") {
+      botly.sendText({id: senderId, text: "يمكنني ترجمة النصوص فقط 🥺"});
+        } else if (message.message.attachments[0].type == "video") {
+      botly.sendText({id: senderId, text: "يمكنني ترجمة النصوص فقط 🥺"});
+    }
+  /*--------- e n d ---------*/
+//botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.TYPING_OFF}); 
+});
+botly.on("postback", async (senderId, message, postback, data, ref) => {
+ //aaaaaaaaaa
+  botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.MARK_SEEN});
+  //aaaaaaaa
+  botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.TYPING_ON});
+    /*--------- s t a r t ---------*/
+  const user = await User.findOne({ where: { uid: senderId}});
+    if (message.postback){ // Normal (buttons)
+    if (postback == "GET_STARTED"){
+        if (user != null) {
+          botly.sendText({id: senderId, text: "سعيد بلقائك مرة اخرى 😄\nانا مستعد لمساعدتك مجددا في الترجمة 😊"});
+        } else {
+            await User.create({ uid: senderId, lang: "en" });
+            botly.sendGeneric({id: senderId, elements: {
+                title: "سعيد بلقائك 😄\nانا هنا لمساعدتك في الترجمة 🥰",
+                image_url: "https://telegra.ph/file/afae3cecb1be747aa78bf.png",
+                subtitle: "انا روبوت للترجمة 😄، يمكنني الترجمة الى لغات متعددة ",
+                buttons: [
+                    botly.createPostbackButton("إبدأ الترجمة 🏁🌐", "ChangeLang"),
+                  botly.createPostbackButton("مطور البوت 🇲🇦😄", "Owner"),
+                ]}, aspectRatio: Botly.CONST.IMAGE_ASPECT_RATIO.HORIZONTAL});
+        }
+    } else if (postback == "Owner") {
+        botly.sendGeneric({id: senderId, elements: {
+           title: "Morocco AI",
+           image_url: "https://telegra.ph/file/6db48bb667028c068d85a.jpg",
+           subtitle: "ولا تنسى متابعة الصفحة ❤️",
+           buttons: [
+              botly.createWebURLButton("صفحة المطور 🇲🇦😄", "https://www.facebook.com/profile.php?id=100090780515885")]},
+            aspectRatio: Botly.CONST.IMAGE_ASPECT_RATIO.HORIZONTAL});
+       } else if (postback == "ChangeLang") {
+        botly.send({
+            "id": senderId,
+            "message": {
+            "text": "اختر اللغة التي تريد الترجمة لها 🔁⚙️",
+            "quick_replies":[
+              {
+                "content_type":"text",
+                "title":"العربية 🇲🇦",
+                "payload":"ar",
+              },{
+                "content_type":"text",
+                "title":"الفرنسية 🇫🇷",
+                "payload":"fr",
+              },{
+                "content_type":"text",
+                "title":"الإنجليزية 🇺🇸",
+                "payload":"en",
+              },{
+                "content_type":"text",
+                "title":"الاسبانية 🇪🇸",
+                "payload":"es",
+              },{
+                "content_type":"text",
+                "title":"الالمانية 🇩🇪",
+                "payload":"de",
+              },{
+                "content_type":"text",
+                "title":"الروسية 🇷🇺",
+                "payload":"ru",
+              },{
+                "content_type":"text",
+                "title":"الايطالية 🇮🇹",
+                "payload":"it",
+              },{
+                "content_type":"text",
+                "title":"التركية 🇹🇷",
+                "payload":"tr",
+              },{
+                "content_type":"text",
+                "title":"الكورية 🇰🇷",
+                "payload":"ko",
+              },{
+                "content_type":"text",
+                "title":"الأندونيسية 🇮🇩",
+                "payload":"id",
+              },{
+                "content_type":"text",
+                "title":"الهندية 🇮🇳",
+                "payload":"hi",
+              },{
+                "content_type":"text",
+                "title":"الالبانية 🇦🇱",
+                "payload":"sq",
+              },{
+                "content_type":"text",
+                "title":"الصينية 🇨🇳",
+                "payload":"zh",
+              }
+            ]
+          }
+          });
+    } else if (postback == "tbs") {
+        //
+    } else if (postback == "bots") {
+      botly.sendText({id: senderId, text: `قائمة روبوتاتنا 🇲🇦😍`,
+      quick_replies: [
+         botly.createQuickReply("Atlas-GPT", "Atlas-GPT")]});
+    }
+  } else { // Quick Reply
+    if (message.message.text == "tbs") {
+        //
+    } else if (message.message.text == "tbs") {
+      //
+    } else if (postback == "ChangeLang"){
+        botly.send({
+            "id": senderId,
+            "message": {
+            "text": "اختر اللغة التي تريد الترجمة لها 🔁⚙️",
+                "quick_replies":[
+                {
+                  "content_type":"text",
+                  "title":"العربية 🇲🇦",
+                  "payload":"ar",
+                },{
+                  "content_type":"text",
+                  "title":"الفرنسية 🇫🇷",
+                  "payload":"fr",
+                },{
+                  "content_type":"text",
+                  "title":"الانجليزية 🇺🇸",
+                  "payload":"en",
+                },{
+                  "content_type":"text",
+                  "title":"الاسبانية 🇪🇸",
+                  "payload":"es",
+                },{
+                  "content_type":"text",
+                  "title":"الالمانية 🇩🇪",
+                  "payload":"de",
+                },{
+                  "content_type":"text",
+                  "title":"الروسية 🇷🇺",
+                  "payload":"ru",
+                },{
+                  "content_type":"text",
+                  "title":"الايطالية 🇮🇹",
+                  "payload":"it",
+                },{
+                  "content_type":"text",
+                  "title":"التركية 🇹🇷",
+                  "payload":"tr",
+                },{
+                  "content_type":"text",
+                  "title":"الكورية 🇰🇷",
+                  "payload":"ko",
+                },{
+                  "content_type":"text",
+                  "title":"الاندونيسية 🇮🇩",
+                  "payload":"id",
+                },{
+                  "content_type":"text",
+                  "title":"الهندية 🇮🇳",
+                  "payload":"hi",
+                },{
+                  "content_type":"text",
+                  "title":"الالبانية 🇦🇱",
+                  "payload":"sq",
+                },{
+                  "content_type":"text",
+                  "title":"الصينية 🇨🇳",
+                  "payload":"zh",
+                }
+            ]
+          }
+          });
+    } else if (postback == "Owner") {
+      botly.sendGeneric({id: senderId, elements: {
+         title: "Morocco AI",
+         image_url: "https://telegra.ph/file/6db48bb667028c068d85a.jpg",
+         subtitle: "صفحة المطور 🇲🇦😄",
+         buttons: [
+            botly.createWebURLButton("مطور البوت 🇲🇦😍", "fb.com/Morocco.Openai")]},
+          aspectRatio: Botly.CONST.IMAGE_ASPECT_RATIO.HORIZONTAL});
+     } else {
+        updateOrCreate(User, { uid: senderId}, {lang: postback})
+        .then(function(result) {
+            botly.sendText({id: senderId, text: "تم تغيير اللغة ✅🌐"});
+          });
+     }
+  }
+   /*--------- e n d ---------*/
+ //aaaaa
+  botly.sendAction({id: senderId, action: Botly.CONST.ACTION_TYPES.TYPING_OFF});
+});
+/*------------- RESP -------------*/
+botly.setGetStarted({pageId: PageID, payload: "GET_STARTED"});
+botly.setGreetingText({
+    pageId: PageID,
+    greeting: [
+      {
+        locale: "default",
+        text: "مترجمي ❤️😄\nبوت يقدم خدمة الترجمة بين لغات مختلفة.\nسعداء باستخدامكم روبوتاتنا ❤️🇲🇦"
+      },
+      {
+        locale: "ar_AR",
+        text: "مترجمي ❤️😄\nبوت يقدم خدمة الترجمة بين لغات مختلفة.\nسعداء باستخدامكم روبوتاتنا ❤️🇲🇦"
+      }
+    ]
+  });
+botly.setPersistentMenu({
+    pageId: PageID,
+    menu: [
+        {
+          locale: "default",
+          composer_input_disabled: false,
+          call_to_actions: [
+            {
+              title:   "تغيير اللغة 🌐🔃",
+              type:    "postback",
+              payload: "ChangeLang"
+            },{
+              type:  "web_url",
+              title: "صفحة المطور 🇲🇦😄",
+              url:   "fb.com/Morocco.Openai/",
+              webview_height_ratio: "full"
+            }
+          ]
+        }
+      ]
+  });
+/*------------- RESP -------------*/
+app.listen(process.env.PORT || port, () =>
+  console.log(`App is on Port : ${port}`)
+)
+
